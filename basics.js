199670a03efe62bf5a535a1fb1b3de93b832db22
Susan <susan@marpy.net>
2015-02-17 11:08:23 -0500

    came in late today, completed quiz

--- basics.js
+++ basics.js
@@ -3,17 +3,21 @@
 // prefixed with the text "I have a " (so "I have a cat", "I have a dog", etc.)
 
 var pets = ['cat', 'dog', 'wombat'];
+pets.map(function(pet){
+  return "I have a " + pet;
+})
 
 ///// Question 2
 // Change my friend's hair color to "green", without modifying the line below.
 
 var friend = { name: 'Chris', age: 27, hairColor: 'blue' };
+  friend.hairColor = 'green';
 
 ///// Question 3
 // The function below is problematic. In 1-2 sentences explain why, and then fix the code.
 
 var squarePlusOne = function(n){
-  squared = n * n;
+  var squared = n * n;
   return squared + 1;
 };
 
@@ -31,6 +35,14 @@ def greet(name, excited)
 end
 */
 
+var greet = function(name, excited){
+  if(excited) {
+    return "Hi, " + name.toUpperCase() + " here!";
+  } else {
+    return "Hi, " + name + " here!";
+  }
+}
+
 ///// Question 5
 // Consider the following code.
 
@@ -41,16 +53,46 @@ var anna = {name: "Anna",
             }
           }
 
-// What is `anna`? (e.g. method, function, string, etc.) 
+// What is `anna`? (e.g. method, function, string, etc.)
 
+//object literal
 
-// What are `name`, `age`, and `luckyNumber`? 
+// What are `name`, `age`, and `luckyNumber`?
 
+//attributes
 
-// What does `this` refer to? 
+// What does `this` refer to?
 
+// 'this' refers to anna
 
-// If you want to return the value of anna's lucky number, how would you do that? 
+// If you want to return the value of anna's lucky number, how would you do that?
 
+  anna.luckyNumber();
 
-// If you want to change anna's name to "wurble", how would you do that? 
+// If you want to change anna's name to "wurble", how would you do that?
+
+  anna.name = "wurble";
+
+// Question 6
+
+// create a Person constructor that takes a name and age as arguments
+
+var Person = function(name, age){
+  this.name = name;
+  this.age = age;
+}
+
+Person.prototype = {
+  luckyNumber: function(){
+    var luckynumber = this.age * 2;
+    return this.name + "'s lucky number is " + luckynumber;
+  }
+}
+
+//initialize two people, david and anna
+
+var david = new Person("David", 30);
+var anna = new Person("Anna", 28);
+
+// create a new property luckyNumber that can be called on all Persons
+
